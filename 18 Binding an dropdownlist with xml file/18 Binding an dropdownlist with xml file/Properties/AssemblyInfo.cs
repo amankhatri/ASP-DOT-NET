602563e40c8d5577a468043086adf8c2b4a935d4
amankhatri <aman_khatri5b5@yahoo.com>
2015-02-05 08:05:55 -0600

    databind, data insert data add and DataSet MapPath
    /*just like using database for storing data we can also use xml to store
    some amount of tata
    
    * ReadXml() method of the DataSet object can be used, to read the data
    from the XML file into a dataset.
    *
    * Server.MapPath() method return the physical path for a given virtual
    path.
    *
    * To insert ListItem at a specific location use Insert() method
    *
    * we have to add all the names of the countries India, US, Australia and
    UK to the drop downlist and
    * we also have to display an addition item "Select", which is not in xml
    file, to the dropdownlist.
    
    */
    protected void Page_Load(object sender, EventArgs e)
    {
    /*To read a xml file, the easiest way is to use Dataset object*/
    DataSet DS = new DataSet();
    /*using this dataset object we can call readxml file, all this data is
    now in DS object by using this*/
    // DS.ReadXml("Contries.xml");
    /*how ever we should also specify the path , that is the relative path
    of the xml file which
    can be mapped using server.mappath method*/
    String PhysicalPath = Server.MapPath("Countries.xml");
    DS.ReadXml(PhysicalPath);
    /*we also have to set the datatext property and data value property for
    dropdownlist*/
    DropDownList1.DataTextField = "CountryName";
    DropDownList1.DataValueField = "CountryId";
    /*we set the data source */
    DropDownList1.DataSource = DS;
    /*we bind the data*/
    DropDownList1.DataBind();
    /*A dropdownllist is a collection of ListItem, so we can create a
    listItem object */
    ListItem li = new ListItem("Select", "-1");
    /*we use -1 as a value because it denotes that useer didn't select
    anything*/
    /*now we can add the item in the DropDownList, however this method adds
    the element in the end
    DropDownList1.Items.Add(li); so we will use DropdownList1.Items.insert
    because then we can specify
    where to add item, that is on what index*/
    DropDownList1.Items.Insert(0, li);
    
    }

---
+++ 18 Binding an dropdownlist with xml file/18 Binding an dropdownlist with xml file/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("_18_Binding_an_dropdownlist_with_xml_file")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("_18_Binding_an_dropdownlist_with_xml_file")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b876c068-e87f-4efe-90d6-67d848a4f300")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
